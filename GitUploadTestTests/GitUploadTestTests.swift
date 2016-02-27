4c7691a7fb3243d0ac9bbb4e090b89640019bbbe
zhaolei <zhaolei@allinmd.cn>
2016-02-27 23:00:11 +0800

    init project

---
+++ GitUploadTestTests/GitUploadTestTests.swift
@@ -0,0 +1,36 @@
+//
+//  GitUploadTestTests.swift
+//  GitUploadTestTests
+//
+//  Created by wdxgtsh on 16/2/27.
+//  Copyright © 2016年 zhaolei. All rights reserved.
+//
+
+import XCTest
+@testable import GitUploadTest
+
+class GitUploadTestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
